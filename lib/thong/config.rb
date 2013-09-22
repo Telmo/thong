ff216b2078bbb5150434e8b0fad96df8f9592d49
Roberto <roberto_moral@cable.comcast.com>
2013-09-22 08:43:11 -0400

    Adding configuration

---
+++ lib/thong/config.rb
@@ -0,0 +1,59 @@
+module Thong
+  module Configuration
+    require 'yaml'
+
+    def self.configuration
+      config = {
+        :git => git,
+      }
+      if yml = self.yaml_config
+        config[:configuratiom] = yml
+        config[:thong_url] = yml.fetch(:thong_url) { "http://localhost:3000" }
+      else
+        puts "[Thong] No configuration file found. Will not send data anywhere.".colorize(:red)
+      end
+      config
+    end
+
+    def self.yaml_config
+      if self.configuration_file && File.exists?(self.configuration_file)
+        YAML::load_file(self.configuration_file)
+      end
+    end
+
+    def self.config_file
+      File.expand_path(File.join(Dir.pwd, ".thong.yml"))
+    end
+
+    def self.git
+      git_hash = {}
+
+      git_hash[:head] = {
+        :id => `git log -1 --pretty=format:'%H'`,
+        :author_name => `git log -1 --pretty=format:'%aN'`,
+        :author_email => `git log -1 --pretty=format:'%ae'`,
+        :committer_name => `git log -1 --pretty=format:'%cN'`,
+        :committer_email => `git log -1 --pretty=format:'%ce'`,
+        :message => `git log -1 --pretty=format:'%s'`
+      }
+
+      branch = `git branch`.split("\n").delete_if { |i| i[0] != "*" }
+      git_hash[:branch] = [branch].flatten.first.gsub("* ","")
+
+      remote = nil
+      begin
+        remotes = `git remote -v`.split(/\n/).map do |remote|
+          splits = remote.split(" ").compact
+          {:name => splits[0], :url => splits[1]}
+        end.uniq
+      rescue
+      end
+
+      git_hash[:remotes] = remotes
+
+      git_hash
+
+    end
+
+  end
+end
